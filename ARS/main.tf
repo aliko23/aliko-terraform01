c2f0704e47975adfab4966263255f5aaff6e1164
aliko23 <aliko23@gmail.com>
2023-09-06 18:33:46 +0300

    Modules02

--- ARS/main.tf
+++ ARS/main.tf
@@ -1,6 +1,6 @@
 #Recovery Services Vault
 resource "azurerm_recovery_services_vault" "ARS" {
-  name                = "${var.resource_group_name}-BackupVault"
+  name                = "${var.resource_group_name}-BackupVault01"
   resource_group_name = var.resource_group_name
   location            = var.location
   sku                 = "Standard"
